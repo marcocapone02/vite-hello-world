0c0dbf61f1a1b82c4758c72dd2432d325dca6fa6
Marco Capone <118698117+marcocapone02@users.noreply.github.com>
2023-05-04 03:56:12 +0200

    AppTitle add

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'AppTitle',
+  data(){
+    return{
+      title:'La mia prima app con Vite!'
+    }
+  }
+  
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
